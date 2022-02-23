75de69c74e3517cd323fdbcd668531b7c03033eb
aleksey pavlov <alekseyelit1337@gmail.com>
2022-02-23 15:36:51 +0300

    контейнер сервисов готов

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -5,15 +5,42 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Bookinist
 {
     public partial class App : Application
     {
-        private IHost __Host;
+        private static IHost __Host;
 
-        public IHost Host => __Host
-        ??= __Ho
+        public static IHost Host => __Host
+            ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] agrs) => Microsoft.Extensions.Hosting.Host
+           .CreateDefaultBuilder(agrs)
+           .ConfigureServices(ConfigureServices)
+        ;
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await Host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            var host = Host;
+            base.OnExit(e);
+            await Host.StopAsync()
+        }
     }
 }
